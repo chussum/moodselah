840b93d89fb258b4f78e06724d10e359c4688055
andrew <andrew@okcoinkr.com>
2019-03-19 14:24:26 +0900

    Fixed bugs.

--- moodselah-server/src/index.ts
+++ moodselah-server/src/index.ts
@@ -1,5 +1,5 @@
 import dotenv from "dotenv";
-dotenv.config({ path: '../.env' });
+dotenv.config();
 
 import { Options } from "graphql-yoga";
 import { createConnection, getConnection } from "typeorm";
